4b1397895759d4524ada1174d2621b15c2bd6b5b
Alone X <aravindyt001@gmail.com>
2022-02-08 18:53:48 +0530

    Update chat_status.py

--- MashaRoBot/modules/helper_funcs/chat_status.py
+++ MashaRoBot/modules/helper_funcs/chat_status.py
@@ -139,7 +139,7 @@ def sudo_plus(func):
                 pass
         else:
             update.effective_message.reply_text(
-                "Who dis non-admin telling me what to do? You want a punch?"
+                ""
             )
 
     return is_sudo_plus_func
