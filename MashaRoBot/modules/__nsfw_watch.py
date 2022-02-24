8bb0607cc2ade503645795a941466f1e6d6f45ac
Alone X <aravindyt001@gmail.com>
2022-02-24 08:03:24 +0530

    Update __nsfw_watch.py

--- MashaRoBot/modules/__nsfw_watch.py
+++ MashaRoBot/modules/__nsfw_watch.py
@@ -111,9 +111,9 @@ async def ws(event):
 
 
 __help__ = """
-Masha can protect your group from NSFW senders
+Alone God can protect your group from NSFW senders
  ❍ /addnsfw*:* Adds The Group to nsfw Watch List
  ❍ /rmnsfw*:* Removes The Group From nsfw Watch List
 """
 
-__mod_name__ = "NSFW"
+__mod_name__ = "Nsғᴡ"
