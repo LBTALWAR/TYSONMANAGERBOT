29a860d77b9c21dea1326cf58068a96b850f2154
Alone X <aravindyt001@gmail.com>
2022-02-24 16:21:45 +0530

    Update misc.py

--- MashaRoBot/modules/helper_funcs/misc.py
+++ MashaRoBot/modules/helper_funcs/misc.py
@@ -80,12 +80,12 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     # can only have a certain amount of buttons side by side
     if len(pairs) > 8:
         pairs = pairs[modulo_page * 10:10 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("ʙᴀᴄᴋ", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="masha_back"),
-             EqInlineKeyboardButton("ɴᴇxᴛ", callback_data="{}_next({})".format(prefix, modulo_page)))]
+            (EqInlineKeyboardButton("Bᴀᴄᴋ", callback_data="{}_prev({})".format(prefix, modulo_page)),
+                EqInlineKeyboardButton("Cʟᴏsᴇ", callback_data="masha_back"),
+             EqInlineKeyboardButton("Nᴇxᴛ", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="masha_back")]]
+        pairs += [[EqInlineKeyboardButton("Cʟᴏsᴇ", callback_data="masha_back")]]
 
     return pairs
 
