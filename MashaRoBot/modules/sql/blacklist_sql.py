09fa8aedc5e3d7797e83062086ba5b23b134244a
Alone X <95560268+TheAloneX@users.noreply.github.com>
2022-05-01 20:46:33 +0530

    Update blacklist_sql.py

--- MashaRoBot/modules/sql/blacklist_sql.py
+++ MashaRoBot/modules/sql/blacklist_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import func, distinct, Column, String, UnicodeText, Integer
 
-from MashaRoBot.modules.sql import SESSION, BASE
+from AloneGodRoBot.modules.sql import SESSION, BASE
 
 
 class BlackListFilters(BASE):
