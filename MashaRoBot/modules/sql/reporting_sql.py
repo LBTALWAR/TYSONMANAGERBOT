0834a612b131559edfe4b54550c62c2324d91ec1
Alone X <95560268+TheAloneX@users.noreply.github.com>
2022-05-01 20:53:16 +0530

    Update reporting_sql.py

--- MashaRoBot/modules/sql/reporting_sql.py
+++ MashaRoBot/modules/sql/reporting_sql.py
@@ -1,7 +1,7 @@
 import threading
 from typing import Union
 
-from MashaRoBot.modules.sql import BASE, SESSION
+from AloneGodRoBot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String
 
 
