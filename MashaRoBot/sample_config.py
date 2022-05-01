f45b36301fff43d28df2c2418a76597fe1c9a4a7
Alone X <95560268+TheAloneX@users.noreply.github.com>
2022-05-01 20:35:32 +0530

    Update sample_config.py

--- MashaRoBot/sample_config.py
+++ MashaRoBot/sample_config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/MashaRoBot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/AloneGodRoBot/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
@@ -17,14 +17,14 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Sawada"
-    SUPPORT_CHAT = "OnePunchSupport"  # Your own group for support, do not add the @
+    OWNER_ID = 5259108841  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "TheAloneXD"
+    SUPPORT_CHAT = "AloneGodSupport"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001253661229
+        -123456789
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001190806654
+        -123456789
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
