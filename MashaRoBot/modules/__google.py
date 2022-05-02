23073629e08b1c57481ad560710bfd72d7b5f2f7
Alone X <95560268+TheAloneX@users.noreply.github.com>
2022-05-02 08:59:44 +0530

    Update __google.py

--- MashaRoBot/modules/__google.py
+++ MashaRoBot/modules/__google.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import urllib
-from MashaRoBot import telethn as tbot
+from AloneGodRoBot import telethn as tbot
 import glob
 import io
 import os
@@ -21,9 +21,9 @@ from telethon.tl import functions
 from telethon.tl import types
 from telethon.tl.types import *
 
-from MashaRoBot import *
+from AloneGodRoBot import *
 
-from MashaRoBot.events import register
+from AloneGodRoBot.events import register
 
 opener = urllib.request.build_opener()
 useragent = "Mozilla/5.0 (Linux; Android 9; SM-G960F Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.157 Mobile Safari/537.36"
@@ -267,7 +267,7 @@ async def apk(e):
             + app_link
             + "'>View in Play Store</a>"
         )
-        app_details += "\n\n===> Masha <==="
+        app_details += "\n\n===> Alone God <==="
         await e.reply(app_details, link_preview=True, parse_mode="HTML")
     except IndexError:
         await e.reply("No result found in search. Please enter **Valid app name**")
